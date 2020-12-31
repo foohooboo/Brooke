afb578400f0677c341a98fb7df261335b76a115e
foohooboo <danielmarshallevans@gmail.com>
2020-12-31 00:47:40 -0700

    parameterized proceed function to give addresses

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-function displayAnswer1() {
+const addresses = [
+  {name: "truck", address: "Look at the front bumper of your car."},
+  {name: "temple", address: "11022 s 4000 w, South Jordan"},
+  {name: "oval", address: "5662 s Cougar Lane, Kearns"},
+  {name: "bowling", address: ""},
+  {name: "copperton", address: "8700 w 10305 s, Copperton"},
+  {name: "pancakes", address: "Look at your stove"},
+]
+
+// const simpleAddresses = {
+//   truck: "Look at the front bumper of your car.",
+//   temple: "11022 s 4000 w, South Jordan",
+//   oval: "5662 s Cougar Lane, Kearns",
+//   bowling: "",
+//   copperton: "8700 w 10305 s, Copperton",
+//   pancakes: "Look at your stove",
+// }
+
+function displayAnswer1(pageName) {
   if (document.getElementById('option-11').checked) {
     document.getElementById('result-11').style.color = 'limegreen'
     document.getElementById('result-11').innerHTML = 'Correct!'
-    proceed()
+    proceed(pageName)
   }
   if (document.getElementById('option-12').checked) {
     document.getElementById('result-12').style.color = 'red'
@@ -18,7 +36,7 @@ function displayAnswer1() {
   }
 }
 
-function displayAnswer2() {
+function displayAnswer2(pageName) {
   if (document.getElementById('option-11').checked) {
     document.getElementById('result-11').style.color = 'red'
     document.getElementById('result-11').innerHTML = 'Incorrect!'
@@ -26,7 +44,7 @@ function displayAnswer2() {
   if (document.getElementById('option-12').checked) {
     document.getElementById('result-12').style.color = 'limegreen'
     document.getElementById('result-12').innerHTML = 'Correct!'
-    proceed()
+    proceed(pageName)
   }
   if (document.getElementById('option-13').checked) {
     document.getElementById('result-13').style.color = 'red'
@@ -38,7 +56,7 @@ function displayAnswer2() {
   }
 }
 
-function displayAnswer3() {
+function displayAnswer3(pageName) {
   if (document.getElementById('option-11').checked) {
     document.getElementById('result-11').style.color = 'red'
     document.getElementById('result-11').innerHTML = 'Incorrect!'
@@ -50,7 +68,7 @@ function displayAnswer3() {
   if (document.getElementById('option-13').checked) {
     document.getElementById('result-13').style.color = 'limegreen'
     document.getElementById('result-13').innerHTML = 'Correct!'
-    proceed()
+    proceed(pageName)
   }
   if (document.getElementById('option-14').checked) {
     document.getElementById('result-14').style.color = 'red'
@@ -58,7 +76,7 @@ function displayAnswer3() {
   }
 }
 
-function displayAnswer4() {
+function displayAnswer4(pageName) {
   if (document.getElementById('option-11').checked) {
     document.getElementById('result-11').style.color = 'red'
     document.getElementById('result-11').innerHTML = 'Incorrect!'
@@ -74,14 +92,15 @@ function displayAnswer4() {
   if (document.getElementById('option-14').checked) {
     document.getElementById('result-14').style.color = 'limegreen'
     document.getElementById('result-14').innerHTML = 'Correct!'
-    proceed()
+    proceed(pageName)
   }
 }
 
 
 //this is the function that will be called on a correct answer selection. It will just display a link at the bottom of the screen. You can use this link to take her to the next URL of the next question or something
-function proceed() {
-  window.alert('some new address goes here');
+function proceed(pageName) {
+  let addr = addresses.find(add => add.name === pageName)
+  window.alert(addr.address);
 }
 
 function checkCodeTruck() {
