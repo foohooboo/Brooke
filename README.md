e75a10588b537df206473880ceb27454829e2394
foohooboo <danielmarshallevans@gmail.com>
2020-12-30 23:01:43 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Dude here is what I had time to come up with. It is pretty static but links will appear when the correct answer is selected.
+
+You can copy the pages and change the links to link to the next URL upon a correct answer
+
+In order to embed your video onto each page, upload it to your youtube account and select 'Share -> embed video -> copy the embed link and replace the entire iframe tag in the video div
+
+# ForDanny
---
+++ script.js
@@ -0,0 +1,36 @@
+//here is an example function to display the correct results. You can change which answer is correct by simply changing what the output of each selection is. This is for question 1. You will need to copy this funcion for each additional question. I know....messy. But I copied it from the internets and didnt have time to make it actually good.
+
+function displayAnswer1() {
+    if (document.getElementById('option-11').checked) {
+      document.getElementById('block-11').style.border = '3px solid limegreen'
+      document.getElementById('result-11').style.color = 'limegreen'
+      document.getElementById('result-11').innerHTML = 'Correct!'
+      proceed()
+    }
+    if (document.getElementById('option-12').checked) {
+      document.getElementById('block-12').style.border = '3px solid red'
+      document.getElementById('result-12').style.color = 'red'
+      document.getElementById('result-12').innerHTML = 'Incorrect!'
+    }
+    if (document.getElementById('option-13').checked) {
+      document.getElementById('block-13').style.border = '3px solid red'
+      document.getElementById('result-13').style.color = 'red'
+      document.getElementById('result-13').innerHTML = 'Incorrect!'
+    }
+    if (document.getElementById('option-14').checked) {
+      document.getElementById('block-14').style.border = '3px solid red'
+      document.getElementById('result-14').style.color = 'red'
+      document.getElementById('result-14').innerHTML = 'Incorrect!'
+    }
+  }
+
+
+//this is the function that will be called on a correct answer selection. It will just display a link at the bottom of the screen. You can use this link to take her to the next URL of the next question or something
+function proceed() {
+    var a = document.createElement('a');
+    var link = document.createTextNode("This is a link");
+    a.appendChild(link);
+    a.title = "This is a Link";
+    a.href = "https://www.geeksforgeeks.org";
+    document.body.appendChild(a);
+}
